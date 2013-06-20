b85e6a85bf200b96cf059c352b6cb60db5524bd6
yoshifumi0521 <yoshifumi0521@gmail.com>
2013-06-20 12:03:26 +0900

    web/css/layout.cssを加えて、config/view.ymlに設定を追加。

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -2,17 +2,35 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 
-<?php include_http_metas() ?>
-<?php include_metas() ?>
+<?php echo include_http_metas() ?>
+<?php echo include_metas() ?>
 
-<?php include_title() ?>
+<?php echo include_title() ?>
 
 <link rel="shortcut icon" href="/favicon.ico" />
 
 </head>
 <body>
 
-<?php echo $sf_data->getRaw('sf_content') ?>
+  <div id="header">
+    <ul>
+      <li><?php echo link_to('about', '@homepage') ?></li>
+      <li>プロフィール</li>
+    </ul>
+    <h1><?php echo link_to(image_tag('askeet_logo.gif', 'alt=askeet'), '@homepage') ?></h1>
+  </div>
+
+  <div id="content">
+    <div id="content_main">
+      <?php echo $sf_data->getRaw('sf_content') ?>
+      <div class="verticalalign"></div>
+    </div>
+
+    <div id="content_bar">
+      <!-- Nothing for the moment -->
+      <div class="verticalalign"></div>
+    </div>
+  </div>
 
 </body>
 </html>
