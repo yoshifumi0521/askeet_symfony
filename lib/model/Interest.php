18790d1a3842b3612e5a44b478caf57c0f8f8cb8
yoshifumi0521 <yoshifumi0521@gmail.com>
2013-06-21 13:33:30 +0900

    Interest.phpで、Interestが保存されるたびに、Questionのinterested_usersを追加するようにする。

--- lib/model/Interest.php
+++ lib/model/Interest.php
@@ -3,10 +3,40 @@
 /**
  * Subclass for representing a row from the 'ask_interest' table.
  *
- * 
+ *
  *
  * @package lib.model
- */ 
+ */
 class Interest extends BaseInterest
 {
+
+    //interesを保存するごとに、Questionのinterested_usersを追加する。
+    public function save($con = null)
+    {
+      $con = Propel::getConnection();
+      try
+      {
+        $con->begin();
+        $ret = parent::save($con);
+
+        // questionテーブルのinterested_usersを更新する
+        $question = $this->getQuestion();
+        $interested_users = $question->getInterestedUsers();
+        $question->setInterestedUsers($interested_users + 1);
+        $question->save($con);
+
+        $con->commit();
+
+        return $ret;
+      }
+      catch (Exception $e)
+      {
+        $con->rollback();
+        throw $e;
+      }
+    }
+
+
+
+
 }
