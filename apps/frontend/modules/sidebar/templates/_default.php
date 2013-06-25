3e2e59c4ff7118432d60ea949f56056a46d043ef
yoshifumi0521 <yoshifumi0521@gmail.com>
2013-06-25 15:04:49 +0900

    サイドバーのコンポーネントを追加

---
+++ apps/frontend/modules/sidebar/templates/_default.php
@@ -0,0 +1,9 @@
+<!--　サイドバーのコンポーネント -->
+<?php echo link_to('ask a new question','question/add') ?>
+
+<ul>
+    <li><?php echo link_to('popular questions', 'question/list') ?></li>
+    <li><?php echo link_to('latest questions', 'question/recent') ?></li>
+    <li><?php echo link_to('latest answers', 'answer/recent') ?></li>
+</ul>
+
